e1a796b054ae039f0e9e37f11f741fe17d2bf08c
Duane Johnson <duane.johnson@gmail.com>
2008-07-20 22:08:10 -0600

    Converted tabs to spaces

--- ruby/get_doc.rb
+++ ruby/get_doc.rb
@@ -7,74 +7,74 @@
 #
 # License: MIT
 begin
-	require 'rubygems'
-	require 'open-uri'
-	require 'hpricot'
-	
-	class GetDoc
-		CACHE_DIR = File.join(File.dirname(__FILE__), "..", "cache")
-		CACHE_REFRESH = 1 * 60 # 1 minute
-		
-		def initialize(docid, url = "http://docs.google.com/View?id=%s", &block)
-			@docid = docid
-			@url = url
-			@transformation = block ||
-				proc do |hpricot|
-					(hpricot / "#doc-contents").inner_html.
-						gsub(/(src|href)="(View\?|File\?)/, "\\1=\"http://docs.google.com/\\2")
-				end
-		end
-		
-		def cache_file
-			File.join(CACHE_DIR, @docid)
-		end
-		
-		def cache_age
-			Time.now - File.ctime(cache_file)
-		end
-		
-		def get_live_doc
-			doc = Hpricot(open(@url % @docid))
-			html = @transformation[doc]
-		rescue OpenURI::HTTPError => e
-			"Document '#{@docid}' not accessible (#{e})"
-		rescue RuntimeError => e
-			if e.to_s[/redirection forbidden/]
-				"Document '#{@docid}' is not public / published."
-			else
-				e.to_s
-			end
-		end
-	
-		def get_cached_doc
-			IO.read(cache_file)
-		rescue Errno::ENOENT
-			"Cached doc '#{@docid}' not found."
-		end
-	
-		def save_doc(html)
-			File.open(cache_file, "w") { |f| f.print html }
-			html
-		rescue Errno::ENOENT, Errno::EACCES
-			"Cache file could not be written: '#{cache_file}'."
-		end
-	
-		def cache_too_old?
-			cache_age > CACHE_REFRESH
-		rescue Errno::ENOENT
-			true
-		end
-	
-		def to_s
-			if cache_too_old?
-				save_doc(get_live_doc)
-			else
-				get_cached_doc
-			end
-		end
-	end
-	
-	""
+  require 'rubygems'
+  require 'open-uri'
+  require 'hpricot'
+  
+  class GetDoc
+    CACHE_DIR = File.join(File.dirname(__FILE__), "..", "cache")
+    CACHE_REFRESH = 1 * 60 # 1 minute
+    
+    def initialize(docid, url = "http://docs.google.com/View?id=%s", &block)
+      @docid = docid
+      @url = url
+      @transformation = block ||
+        proc do |hpricot|
+          (hpricot / "#doc-contents").inner_html.
+            gsub(/(src|href)="(View\?|File\?)/, "\\1=\"http://docs.google.com/\\2")
+        end
+    end
+    
+    def cache_file
+      File.join(CACHE_DIR, @docid)
+    end
+    
+    def cache_age
+      Time.now - File.ctime(cache_file)
+    end
+    
+    def get_live_doc
+      doc = Hpricot(open(@url % @docid))
+      html = @transformation[doc]
+    rescue OpenURI::HTTPError => e
+      "Document '#{@docid}' not accessible (#{e})"
+    rescue RuntimeError => e
+      if e.to_s[/redirection forbidden/]
+        "Document '#{@docid}' is not public / published."
+      else
+        e.to_s
+      end
+    end
+  
+    def get_cached_doc
+      IO.read(cache_file)
+    rescue Errno::ENOENT
+      "Cached doc '#{@docid}' not found."
+    end
+  
+    def save_doc(html)
+      File.open(cache_file, "w") { |f| f.print html }
+      html
+    rescue Errno::ENOENT, Errno::EACCES
+      "Cache file could not be written: '#{cache_file}'."
+    end
+  
+    def cache_too_old?
+      cache_age > CACHE_REFRESH
+    rescue Errno::ENOENT
+      true
+    end
+  
+    def to_s
+      if cache_too_old?
+        save_doc(get_live_doc)
+      else
+        get_cached_doc
+      end
+    end
+  end
+  
+  ""
 rescue Exception => e
-	e.to_s
+  e.to_s
 end
